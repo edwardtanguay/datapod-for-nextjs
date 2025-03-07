9e1b47a7f2d6e4b684c570726cc4cba36bd55737
Edward Tanguay (windows) <edward@tanguay.info>
2025-03-07 05:35:18 +0100

    ver on side

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,9 @@ export const Header = () => {
 	const activeSegment = useSelectedLayoutSegment();
 	return (
 		<div>
-			<div className="flex justify-between py-2 px-4 sm:py-4 sm:px-4">
-				<h1 className="text-xl sm:text-3xl">Datapod for Next.js</h1>
-				<div className="text-xs">0.00.11</div>
+			<div className="flex justify-between items-center py-2 px-4 sm:py-4 sm:px-4">
+				<h1 className="text-xl sm:text-3xl">Datapod for Next.js 15</h1>
+				<div className="text-xs">v 0.00.11</div>
 			</div>
 			<nav className="p-2 mainnav bg-slate-300 pl-4">
 				<ul className="flex gap-x-2 gap-y-0 flex-wrap">
