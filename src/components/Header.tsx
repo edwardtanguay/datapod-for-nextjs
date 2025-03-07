3ef7a242844c83a1ec4c12b5716c2e8af1bc01fe
Edward Tanguay (windows) <edward@tanguay.info>
2025-03-07 05:35:29 +0100

    12

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export const Header = () => {
 		<div>
 			<div className="flex justify-between items-center py-2 px-4 sm:py-4 sm:px-4">
 				<h1 className="text-xl sm:text-3xl">Datapod for Next.js 15</h1>
-				<div className="text-xs">v 0.00.11</div>
+				<div className="text-xs">v 0.00.12</div>
 			</div>
 			<nav className="p-2 mainnav bg-slate-300 pl-4">
 				<ul className="flex gap-x-2 gap-y-0 flex-wrap">
