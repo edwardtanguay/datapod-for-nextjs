97d53723d5285849acf5f4ee47167f071847ddb8
Edward Tanguay (windows) <edward@tanguay.info>
2025-03-07 06:28:16 +0100

    basic items

--- src/app/info/[id]/page.tsx
+++ src/app/info/[id]/page.tsx
@@ -9,9 +9,11 @@ export default function Page({ params }: { params: { id: string } }) {
 
 	return (
 		<div>
-			<p>Info: {id}</p>
 			{info && (
-				<p>Content: {info.content}</p>
+				<div key={info.id} className="p-4 border rounded shadow-sm hover:shadow-md transition-shadow">
+					<h2 className="text-xl font-semibold">Item {info.id}</h2>
+					<p className="mt-2">{info.content}</p>
+				</div>
 			)}
 		</div>
 	);
--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 export default function About() {
 	const infos = [
 		{ id: 1, content: "First item content" },
@@ -7,15 +9,17 @@ export default function About() {
 
 	return (
 		<div className="p-4">
-			<h1 className="text-2xl font-bold mb-4">Info List</h1>
-			<div className="space-y-4">
+			<ul>
 				{infos.map((info) => (
-					<div key={info.id} className="p-4 border rounded shadow-sm hover:shadow-md transition-shadow">
-						<h2 className="text-xl font-semibold">Item {info.id}</h2>
-						<p className="mt-2">{info.content}</p>
-					</div>
+					<li key={info.id}>
+						<Link
+							href={`info/${info.id}`}
+						>
+							{info.content}
+						</Link>
+					</li>
 				))}
-			</div>
+			</ul>
 		</div>
 	);
 }
