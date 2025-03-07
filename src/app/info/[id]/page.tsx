7508315e77a7d29482dfb523d3dd2fb682c724d0
Edward Tanguay (windows) <edward@tanguay.info>
2025-03-07 06:22:37 +0100

    showing id basic

--- src/app/info/[id]/page.tsx
+++ src/app/info/[id]/page.tsx
@@ -1,9 +1,18 @@
+const infos = [
+	{ id: 1, content: "First item content" },
+	{ id: 2, content: "Second item content" },
+	{ id: 3, content: "Third item content" }
+];
 export default function Page({ params }: { params: { id: string } }) {
 	const { id } = params;
+	const info = infos.find(m => m.id === Number(id));
 
 	return (
 		<div>
 			<p>Info: {id}</p>
+			{info && (
+				<p>Content: {info.content}</p>
+			)}
 		</div>
 	);
 }
