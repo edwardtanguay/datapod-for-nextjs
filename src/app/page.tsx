aa43ce98504b2e158c0dc2d6f88c6910a67d0192
Edward Tanguay (windows) <edward@tanguay.info>
2025-03-07 10:05:41 +0100

    shows location

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import * as config from '@/config';
+
 export default function Home() {
   return (
     <div>
       <p>Welcome to this site.</p>
+      <p>{config.getSiteLocation()}</p>
     </div>
   );
 }
--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,9 @@
 export const getVersion = () : string => {
-	return "0.00.25";
+	return "0.00.26";
+}
+
+export const getSiteLocation = () : string => {
+    return process.env.SITE_URL ? 
+        (process.env.SITE_URL.includes('http://localhost') ? 'local' : 'online') 
+        : 'test';
 }
