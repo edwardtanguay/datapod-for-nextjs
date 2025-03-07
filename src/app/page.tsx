5d7abec1da92a6ae48762b46095b5046184c242a
Edward Tanguay (windows) <edward@tanguay.info>
2025-03-07 04:01:34 +0100

    mobile padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="min-h-screen p-0 gap-16 sm:p-12 font-[family-name:var(--font-geist-sans)]">
+    <div className="min-h-screen p-4 gap-16 sm:p-12 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-4 sm:gap-8 row-start-2 sm:items-start">
       <h1 className="text-xl sm:text-2xl">Datapod for Next.js 0.05</h1>
       <p>Welcome to this site.</p>
