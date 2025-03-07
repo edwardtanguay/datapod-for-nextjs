ba3462e40c00ec9e9a77df33b2667dac874a744b
Edward Tanguay (windows) <edward@tanguay.info>
2025-03-07 04:58:47 +0100

    nav styles both

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import Link from "next/link"
 export const Header = () => {
 	return (
 		<div>
-			<h1 className="text-xl sm:text-2xl py-2 px-4">Datapod for Next.js 0.08</h1>
-			<nav className="p-2 mainnav mb-3 bg-slate-300 pl-4">
+			<h1 className="text-xl sm:text-3xl py-2 px-4 sm:py-4 sm:px-4">Datapod for Next.js 0.09</h1>
+			<nav className="p-2 mainnav bg-slate-300 pl-4">
 				<ul className="flex gap-x-2 gap-y-0 flex-wrap">
 					<li>
 						<Link
