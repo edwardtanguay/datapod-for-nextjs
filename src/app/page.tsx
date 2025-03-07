e1a1dd85e7170ce9fee76494c784d6eec782e813
Edward Tanguay (windows) <edward@tanguay.info>
2025-03-07 04:42:57 +0100

    with nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="min-h-screen p-4 gap-16 sm:p-12 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-4 sm:gap-8 row-start-2 sm:items-start">
-      <h1 className="text-xl sm:text-2xl">Datapod for Next.js 0.06</h1>
+      <h1 className="text-xl sm:text-2xl">Datapod for Next.js 0.07</h1>
       <p>Welcome to this site.</p>
       </main>
     </div>
