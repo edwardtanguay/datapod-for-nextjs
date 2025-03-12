23f7baebe35c48aee8b5d7f94a60adb252ec9a23
Edward Tanguay (windows) <edward@tanguay.info>
2025-03-12 22:43:32 +0100

    modif

--- src/app/api/db.ts
+++ src/app/api/db.ts
@@ -1,28 +1,19 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
-const uri =
-	`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.xidme.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-	serverApi: {
-		version: ServerApiVersion.v1,
-		strict: true,
-		deprecationErrors: true,
-	},
-});
+export const connectToDb = async () => {
+	const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.xidme.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-async function run() {
+	const client = new MongoClient(uri, {
+		serverApi: {
+			version: ServerApiVersion.v1,
+			strict: true,
+			deprecationErrors: true,
+		},
+	});
+
 	try {
-		// Connect the client to the server	(optional starting in v4.7)
 		await client.connect();
-		// Send a ping to confirm a successful connection
-		await client.db("admin").command({ ping: 1 });
-		console.log(
-			"Pinged your deployment. You successfully connected to MongoDB!"
-		);
 	} finally {
-		// Ensures that the client will close when you finish/error
 		await client.close();
 	}
-}
-run().catch(console.dir);
+};
