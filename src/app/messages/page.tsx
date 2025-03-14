9e70c867552409eba5cc4eb07b6cf795d382c324
Edward Tanguay (windows) <edward@tanguay.info>
2025-03-14 11:21:11 +0100

    highlighted in

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,14 +1,23 @@
 import { FrontendMessage, Message } from "@/types";
+import { FaStar } from "react-icons/fa6";
 
 export default async function Page() {
 	let messages: Message[] = [];
-  const FrontendMessages: FrontendMessage[] = [];
+	const frontendMessages: FrontendMessage[] = [];
 
 	try {
 		const res = await fetch(`${process.env.SITE_URL}/api/messages`);
 		if (!res.ok) throw new Error("Failed to fetch messages");
 		messages = await res.json();
 
+		for (const message of messages) {
+			const frontendMessage: FrontendMessage = {
+				_id: message._id,
+				text: message.text,
+				highlighted: false,
+			};
+			frontendMessages.push(frontendMessage);
+		}
 	} catch (err) {
 		console.error("Message fetch error:", err);
 		return (
@@ -25,12 +34,15 @@ export default async function Page() {
 				which fetches data from a MongoDB Atlas database.
 			</p>
 			<div className="mt-6 space-y-4">
-				{messages.map((message, index) => (
+				{frontendMessages.map((frontendMessage, index) => (
 					<div
 						key={index}
-						className="p-4 bg-white rounded-lg shadow-sm border border-gray-200"
+						className="p-4 bg-white rounded-lg shadow-sm border border-gray-200 flex gap-2 items-center"
 					>
-						<p className="text-gray-800">{message.text} ({message._id})</p>
+						<p>{frontendMessage.highlighted ? <FaStar className="text-yellow-500" /> : <FaStar className="text-gray-400" />}</p>
+						<p className="text-gray-800">
+							{frontendMessage.text} ({frontendMessage._id})
+						</p>
 					</div>
 				))}
 			</div>
