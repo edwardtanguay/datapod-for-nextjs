f60cb64f430fd8959d1ad389863e7733242f468c
Edward Tanguay (windows) <edward@tanguay.info>
2025-03-14 11:14:32 +0100

    now id

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,33 +1,39 @@
-import { Message } from "@/types";
+import { FrontendMessage, Message } from "@/types";
 
 export default async function Page() {
-  let messages:Message[] = [];
-  try {
-    const res = await fetch(`${process.env.SITE_URL}/api/messages`);
-    if (!res.ok) throw new Error('Failed to fetch messages');
-    messages = await res.json();
-  } catch (err) {
-    console.error('Message fetch error:', err);
-    return (
-      <div className="max-w-4xl mx-auto p-4">
-        <div className="text-red-500 mt-4">Error loading messages</div>
-      </div>
-    );
-  }
+	let messages: Message[] = [];
+  const FrontendMessages: FrontendMessage[] = [];
 
-  return (
-    <div className="max-w-4xl mx-auto p-4">
-	<p>This is a page showing how to get data from a backend route, which fetches data from a MongoDB Atlas database.</p>
-      <div className="mt-6 space-y-4">
-        {messages.map((message, index) => (
-          <div 
-            key={index}
-            className="p-4 bg-white rounded-lg shadow-sm border border-gray-200"
-          >
-            <p className="text-gray-800">{message.text}</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	try {
+		const res = await fetch(`${process.env.SITE_URL}/api/messages`);
+		if (!res.ok) throw new Error("Failed to fetch messages");
+		messages = await res.json();
+
+	} catch (err) {
+		console.error("Message fetch error:", err);
+		return (
+			<div className="max-w-4xl mx-auto p-4">
+				<div className="text-red-500 mt-4">Error loading messages</div>
+			</div>
+		);
+	}
+
+	return (
+		<div className="max-w-4xl mx-auto p-4">
+			<p>
+				This is a page showing how to get data from a backend route,
+				which fetches data from a MongoDB Atlas database.
+			</p>
+			<div className="mt-6 space-y-4">
+				{messages.map((message, index) => (
+					<div
+						key={index}
+						className="p-4 bg-white rounded-lg shadow-sm border border-gray-200"
+					>
+						<p className="text-gray-800">{message.text} ({message._id})</p>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
--- src/types.ts
+++ src/types.ts
@@ -4,7 +4,14 @@ export type Info = {
 }
 
 export type Message = {
+    _id: string;
     text: string;
+}
+
+export type FrontendMessage = {
+    _id: string;
+    text: string;
+    highlighted: boolean;
 }
 
 export type SiteLocation = "local" | "online" | "error";
